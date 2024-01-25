c6c44d07dd94d0390b313c53e5c88f556160fb9b
vmandic <mandic.vedran@gmail.com>
2024-01-26 00:30:13 +0100

    Make it fly.io deployable

--- src/http_server.mjs
+++ src/http_server.mjs
@@ -27,7 +27,6 @@ app.get("/scrape", (req, res) => {
 
 app.get("/settings", (req, res) => {
   const settings = sortObjectByPropertyNames(getSettings());
-  settings.WSS_PRIVATE_KEY = "REDACTED";
   res.send(settings);
 });
 
@@ -41,33 +40,26 @@ app.use((req, res) => {
   res.status(404).send("The page you are looking for does not exist.");
 });
 
-const sslKey = fs.readFileSync("certs/tris-self-signed-key.pem", "utf8");
-// Load SSL certificate and key
-if (!sslKey || !sslKey.startsWith("-----BEGIN PRIVATE KEY-----")) {
-  throw new Error("Invalid private key: " + WSS_PRIVATE_KEY);
-} else {
-  console.log("WSS SSL cert successfully read, applying it to web server configuration");
-}
-
-const sslCert = fs.readFileSync("certs/tris-self-signed-cert.pem", "utf8");
-
-// Create an HTTPS server with the SSL certificate and key
-const httpsServer = https.createServer(
-  {
-    key: sslKey,
-    cert: sslCert,
-  },
-  app
-);
-
-// Create an HTTP server
-const httpServer = http.createServer(app);
+ const httpServer = app.listen(httpPort, () => {
+  console.log(`HTTP server is running on port ${httpPort}`);
+});
 
 // Create a WebSocket server attached to the HTTPS server
 const wss = new WebSocketServer({
-  server: httpsServer,
-  rejectUnauthorized: false,
+  server: httpServer
 });
+
+// Poor man's single-replica anti-abuse :-)
+let scrapingConcurrentCount = 0;
+function resetScrapingConcurrentCount() {
+  if (scrapingConcurrentCount > 0) {
+    setTimeout(() => {
+      if (scrapingConcurrentCount > 0) {
+        scrapingConcurrentCount--;
+      }
+    }, 10000);
+  }
+}
 
 // Handle WebSocket connections
 wss.on("connection", (ws) => {
@@ -84,6 +76,16 @@ wss.on("connection", (ws) => {
       ws.send("Error: Please provide a URL parameter.");
     } else {
       try {
+
+        if (scrapingConcurrentCount > 3) {
+          console.log("Rate limited");
+          ws.send(`Scraper is limited to 3 scraping processes at a time, please try again in 10s`);
+          resetScrapingConcurrentCount();
+          ws.close();
+          return;
+        }
+
+        scrapingConcurrentCount++;
         // TODO: implement a failsafe to unhook on client disconnect
         ws.send(`Starting scraper for URL: ${url}`);
         await startScraping({
@@ -110,13 +112,4 @@ wss.on("connection", (ws) => {
 
 wss.on("error", (error) => {
   console.log("WebSocket Server Error:", error);
-});
-
-// Start the servers
-httpsServer.listen(httpsPort, () => {
-  console.log(`HTTPS/WSS server is running on port ${httpsPort}`);
-});
-
-httpServer.listen(httpPort, () => {
-  console.log(`HTTP server is running on port ${httpPort}`);
 });
